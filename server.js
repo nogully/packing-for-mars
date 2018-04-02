3f4ba935a13ca8a4dbc5ea827b478eab4907e4cb
Nora Gully <nora.gully@gmail.com>
2018-04-02 09:25:03 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Mars Packing List';
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+  response.send('Oh hi');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
